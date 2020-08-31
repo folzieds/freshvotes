52588d6ba8889ec167c1caea4873ced1b181fc39
Adefolarin Osu <aosu@teamapt.com>
2020-08-31 23:00:18 +0100

    init commit

---
+++ src/main/java/com/phos/freshvotes/FreshvotesappApplication.java
@@ -0,0 +1,13 @@
+package com.phos.freshvotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshvotesappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreshvotesappApplication.class, args);
+    }
+
+}
