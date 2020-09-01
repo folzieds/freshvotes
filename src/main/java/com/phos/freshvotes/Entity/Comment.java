2dc8a868e8c8e5779475217538c5d80bad8ed20c
Adefolarin Osu <aosu@teamapt.com>
2020-09-01 01:19:52 +0100

    Added Entity package which includes the base Entities

---
+++ src/main/java/com/phos/freshvotes/Entity/Comment.java
@@ -0,0 +1,35 @@
+package com.phos.freshvotes.Entity;
+
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+
+/**
+ * Created by Folarin on 01/09/2020
+ */
+
+@Entity
+public class Comment {
+
+    @EmbeddedId
+    private CommentId pk;
+
+    @Column(length = 5000)
+    private String text;
+
+    public CommentId getPk() {
+        return pk;
+    }
+
+    public void setPk(CommentId pk) {
+        this.pk = pk;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
---
+++ src/main/java/com/phos/freshvotes/Entity/CommentId.java
@@ -0,0 +1,35 @@
+package com.phos.freshvotes.Entity;
+
+import javax.persistence.Embeddable;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+
+/**
+ * Created by Folarin on 01/09/2020
+ */
+
+@Embeddable
+public class CommentId implements Serializable {
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Feature feature;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Feature getFeature() {
+        return feature;
+    }
+
+    public void setFeature(Feature feature) {
+        this.feature = feature;
+    }
+}
