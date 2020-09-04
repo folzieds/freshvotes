dbd073364c0d909655dc55578262fb8aa97ccc1c
Adefolarin Osu <aosu@teamapt.com>
2020-09-04 08:45:56 +0100

    Added Test to check the password encoder. resolved recussive issue of customSecurityUser

---
+++ src/test/java/com/phos/freshvotes/service/UserDetailsServiceTest.java
@@ -0,0 +1,23 @@
+package com.phos.freshvotes.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ * Created by Folarin on 04/09/2020
+ */
+public class UserDetailsServiceTest {
+
+    @Test
+    public void test(){
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+        String rawPassword = "password";
+        String encodedPassword = encoder.encode(rawPassword);
+
+        System.out.println(encodedPassword);
+
+        Assert.assertNotEquals(rawPassword,encodedPassword);
+    }
+}
