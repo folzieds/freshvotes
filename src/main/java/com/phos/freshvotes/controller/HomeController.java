d691aba80063130720263273966f6114e29f9276
Adefolarin Osu <folzieds@gmail.com>
2020-09-27 22:29:22 +0100

    Refactored dashboard html page to show all the products for a user and added a controller

--- src/main/java/com/phos/freshvotes/controller/HomeController.java
+++ src/main/java/com/phos/freshvotes/controller/HomeController.java
@@ -1,6 +1,11 @@
 package com.phos.freshvotes.controller;
 
+import com.phos.freshvotes.Entity.User;
+import com.phos.freshvotes.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
@@ -10,13 +15,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    @Autowired
+    ProductService productService;
+
     @GetMapping("/")
     public String home(){
         return "index";
     }
 
     @GetMapping("/dashboard")
-    public String dashboard(){
+    public String dashboard(@AuthenticationPrincipal User user, Model model){
+        model.addAttribute("products",productService.getProductByUsername(user));
         return "dashboard";
     }
 }
