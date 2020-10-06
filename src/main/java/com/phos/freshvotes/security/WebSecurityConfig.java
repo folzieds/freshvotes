777066e72edb7a3013c04032be8ca20abab50533
Adefolarin Osu <folzieds@gmail.com>
2020-10-06 11:40:25 +0100

    Added a logo and icon

--- src/main/java/com/phos/freshvotes/security/WebSecurityConfig.java
+++ src/main/java/com/phos/freshvotes/security/WebSecurityConfig.java
@@ -37,6 +37,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                     .antMatchers("/").permitAll()
                     .antMatchers("/register").permitAll()
+                    .antMatchers("/images/**").permitAll()
                     .anyRequest().hasRole("USER").and()
                 .formLogin()
                     .loginPage("/login")
